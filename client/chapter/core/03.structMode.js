d54b5a2a333f4741b9f6b5a361e866c04cf527e7
hamjongseok <irang4605@naver.com>
2023-01-13 09:20:02 +0900

    흐음

--- client/chapter/core/03.structMode.js
+++ client/chapter/core/03.structMode.js
@@ -2,12 +2,14 @@
 /* Strict Mode                                                            */
 /* ---------------------------------------------------------------------- */
 
+type = "zero";
 
-// 엄격 모드를 사용한 코드와 그렇지 않은 코드를 비교해봅니다.
+let type1 = { type : "one"};
 
-// #1
-jujeob = '심선범 넌 뭐랄까.. 마치 베를린 같아. 왜냐하면 치명적인 독일 수도.';
-console.log(jujeob);
+let type2 = { type : "two"};
 
-// #2
-// console.log(this);
+function getType() {
+	console.log(this.type);
+}
+
+getType();
